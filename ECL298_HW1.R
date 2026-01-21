eaa83080c6f4486b8786bb1f48e63783fc85699e
pciptaningsih <pciptaningsih@ucdavis.edu>
2026-01-20 16:33:36 -0800

    Delete ECL298_HW1.R

--- ECL298_HW1.R
+++
@@ -1,6 +0,0 @@
-#ECL 298 Prediction 
-#Name: Priska Ciptaningsih
-#Homework 1: Regression and Cross Validation
-#Assignment Details: Please write an R script that shows a form of regression and *k*-fold cross-validation 
-#using one of the example data sets from ISLR (or another dataset that is easily accessible with your code)
-
